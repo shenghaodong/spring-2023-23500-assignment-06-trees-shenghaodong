6d4503dd326abb60c698dbc66886ff920a4d06a2
Shenghao <shenghaodong18@gmail.com>
2023-05-08 14:20:06 -0400

    added node files

---
+++ tree.cpp
@@ -0,0 +1,10 @@
+/*
+What Pretty Tree Printer should look like: Use this to find the format. Tree size may affect how much spaces is needed
+Probably not going to do this seems kind of hard
+
+             0
+           /   \
+          1     2
+         / \   / \
+        3   4 5   6
+*/
